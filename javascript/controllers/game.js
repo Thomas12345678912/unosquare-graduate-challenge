573cb1418c48a0a64b5c4c95f3833f7d1f36bf5e
Thomas12345678912 <mckeethomas293@gmail.com>
2023-05-26 15:12:33 +0100

    updates, added delete query

--- javascript/controllers/game.js
+++ javascript/controllers/game.js
@@ -3,7 +3,7 @@ const { v4: uuid } = require("uuid");
 const words = ["Banana", "Canine", "Unosquare", "Airport"];
 const games = {};
 
-const retrieveWord = () => words[Math.floor(Math.random(words.length - 1))];
+const retrieveWord = () => words[Math.floor(Math.random() * words.length)];
 
 const clearUnmaskedWord = (game) => {
     const withoutUnmasked = { 
@@ -27,52 +27,69 @@ function createGame(req, res) {
   
     games[gameId] = newGame;
   
-    res.send(gameId)
+   
+    res.send(gameId);
     
   }
   
   function getGame(req, res) { 
     const gameId = req.params.gameId;   
+    
+   //create a first input for the user
+   //plan, make fancy ejs page
+
     if (!gameId) return res.sendStatus(404);
   
     var game = games[gameId];  
     if (!game) {
       return res.sendStatus(404); 
     }
-  
-    res.status(200).json(clearUnmaskedWord(game));
-  }
+
+
   
 
-function createGuess(req, res) { 
+    res.status(200).json({
+        game: clearUnmaskedWord(game),
+     
+      });
+    } 
+  
+
+  function createGuess(req, res) {
     const gameId = req.params.gameId;
     let { letter } = req.body;
 
     if (!gameId) return res.sendStatus(404);
 
-    var game = games[gameId]; 
-   
-  
-    if (!game) return res.sendStatus(404); 
-    if (!letter || letter.length != 1) {
+    var game = games[gameId];
+    if (!game) return res.sendStatus(404);
+    if (game.status !== "In Progress") {
+        return res.status(400).json({
+          Message: "Cannot make a guess. The game is not in progress.",
+        });
+      }
+    if (!letter || letter.length !== 1) {
         return res.status(400).json({
             Message: "Guess must be supplied with 1 letter"
-        })
-    }
-     
-    // todo: add logic for making a guess, modifying the game and updating the status
- 
-    letter = letter.toUpperCase() && letter; 
-    if (game.incorrectGuesses.includes(letter) || game.word.includes(letter)) {
-        return res.status(400).json({
-            Message: "This letter has already been guessed"
-        })
+        });
     }
 
-    if (game.unmaskedWord.includes(letter)) {
-        for (let loop = 0; loop < game.unmaskedWord.length; loop++) {
-            if (game.unmaskedWord[loop] === letter) {
-                game.word = game.word.substr(0, loop) + letter + game.word.substr(loop + 1);
+    // Convert the letter to lowercase or uppercase
+    letter = letter.toLowerCase(); // or letter = letter.toUpperCase();
+
+    // Convert the game word to lowercase or uppercase
+    const unmaskedWord = game.unmaskedWord.toLowerCase(); // or game.unmaskedWord.toUpperCase();
+
+    if (game.incorrectGuesses.includes(letter) || game.word.toLowerCase().includes(letter)) {
+        return res.status(400).json({
+            Message: "This letter has already been guessed"
+        });
+    }
+
+    if (unmaskedWord.includes(letter)) {
+        for (let loop = 0; loop < unmaskedWord.length; loop++) {
+            if (unmaskedWord[loop] === letter) {
+                game.word = game.word.substr(0, loop) + game.unmaskedWord[loop] + game.word.substr(loop + 1);
             }
         }
     } else {
@@ -82,15 +99,35 @@ function createGuess(req, res) {
 
     if (game.remainingGuesses === 0) {
         game.status = "Lost";
+        game.word = game.unmaskedWord;
     } else if (!game.word.includes('_')) {
         game.status = "Won";
     }
 
     return res.status(200).json(clearUnmaskedWord(game));
 }
+function deleteGame(req, res) {
+    const gameId = req.params.gameId;
+  
+    if (!gameId || !games[gameId]) {
+      return res.status(404).json({ message: "Game not found" });
+    }
+  
+    const game = games[gameId];
+    if (game.status !== "Won" && game.status !== "Lost") {
+      return res.status(400).json({ message: "Game is not completed" });
+    }
+  
+    delete games[gameId];
+    return res.status(200).json({ message: "Game deleted successfully", gameId });
+}
 
-module.exports = {
+
+  
+  module.exports = {
     createGame,
     getGame,
     createGuess,
+    deleteGame,
   };
+  
--- javascript/index.js
+++ javascript/index.js
@@ -3,7 +3,10 @@ const cors = require("cors");
 const morgan = require("morgan");
 const { json, urlencoded } = require("body-parser");
 const gamesController = require("./controllers/game");
-const axios = require('axios');
+const path = require('path');
+
+
+
 
 const app = express();
 
@@ -13,17 +16,22 @@ app.use(morgan("tiny"));
 app.use(urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
-  res.send("Hello World");
-});
+    const startGame = `<form action="/games" method="get">
+                          <button type="submit">start</button>
+                        </form>`;
+    res.send(startGame);
+  });
+  app.post("/games", gamesController.createGame);
 
-app.post("/games", gamesController.createGame)
 
 app.get("/games/:gameId", gamesController.getGame);
 
 
     app.post("/games/:gameId/guesses", gamesController.createGuess)
     
-      
+    app.delete("/games/:gameId", gamesController.deleteGame);
+
+
       
 app.listen(4567, () => {
   console.log("Server listening at http://localhost:4567");
